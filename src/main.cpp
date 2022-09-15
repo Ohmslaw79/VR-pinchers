981ad9472c6746f8141709b5847ee64b819948db
Ohmslaw79 <noahtheswimmer79@gmail.com>
2022-09-15 14:20:00 -0400

    Framework main moved into src folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <Arduino.h>
 
 void setup() {
+  int pot1_pin;
+  int pot2_pin;
+  int pot1_val;
+  int pot2_val;
+
+  int servo1_pin;
+  int servo2_pin;
+  int servo1_pos = 0;
+  int servo2_pos = 0;
+
+  
   // put your setup code here, to run once:
 }
 
